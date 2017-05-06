f46a23e967b33e7deb4b2ecbf021b2c7223ccc42
Aakash Shah <aakassshah@gmail.com>
2017-05-05 20:39:28 -0400

    topology.py: Changed and modified the code for our topology

--- topology.py
+++ topology.py
@@ -1,60 +1,72 @@
 #!/usr/bin/python
 
-from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import Host
 from mininet.node import OVSKernelSwitch
 from mininet.topo import Topo
 
-class fatTreeTopo(Topo):
 
-    "Fat Tree Topology"
+class FatTreeTopo(Topo):
+
+    """ Switch-Routed (Fat Tree) Topology """
 
     def __init__(self):
-        "Create Fat tree Topology"
+
+        """ Constructor for creating the Switched Routed Topology """
 
         Topo.__init__(self)
 
-        #Add hosts
-        h7 = self.addHost('h7', cls=Host, ip='10.0.0.7', defaultRoute=None)
-        h8 = self.addHost('h8', cls=Host, ip='10.0.0.8', defaultRoute=None)
-        h1 = self.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
-        h2 = self.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
-        h4 = self.addHost('h4', cls=Host, ip='10.0.0.4', defaultRoute=None)
-        h3 = self.addHost('h3', cls=Host, ip='10.0.0.3', defaultRoute=None)
-        h5 = self.addHost('h5', cls=Host, ip='10.0.0.5', defaultRoute=None)
-        h6 = self.addHost('h6', cls=Host, ip='10.0.0.6', defaultRoute=None)
+        # Adding the ToR Switches
+        switch1 = self.addSwitch('switch1', cls=OVSKernelSwitch)
+        switch2 = self.addSwitch('switch2', cls=OVSKernelSwitch)
+        switch3 = self.addSwitch('switch3', cls=OVSKernelSwitch)
+        switch4 = self.addSwitch('switch4', cls=OVSKernelSwitch)
 
-        #Add switches
-        s10 = self.addSwitch('s10', cls=OVSKernelSwitch)
-        s3 = self.addSwitch('s3', cls=OVSKernelSwitch)
-        s17 = self.addSwitch('s17', cls=OVSKernelSwitch)
-        s4 = self.addSwitch('s4', cls=OVSKernelSwitch)
-        s18 = self.addSwitch('s18', cls=OVSKernelSwitch)
-        s1 = self.addSwitch('s1', cls=OVSKernelSwitch)
-        s11 = self.addSwitch('s11', cls=OVSKernelSwitch)
-        s21 = self.addSwitch('s21', cls=OVSKernelSwitch)
-        s22 = self.addSwitch('s22', cls=OVSKernelSwitch)
-        s2 = self.addSwitch('s2', cls=OVSKernelSwitch)
+        # Adding the Aggregate Switches
+        switch21 = self.addSwitch('switch21', cls=OVSKernelSwitch)
+        switch10 = self.addSwitch('switch10', cls=OVSKernelSwitch)
+        switch11 = self.addSwitch('switch11', cls=OVSKernelSwitch)
+        switch22 = self.addSwitch('switch22', cls=OVSKernelSwitch)
 
-        #Add links
-        self.addLink(h1, s1)
-        self.addLink(h2, s1)
-        self.addLink(h3, s2)
-        self.addLink(h4, s2)
-        self.addLink(h5, s3)
-        self.addLink(h6, s3)
-        self.addLink(h7, s4)
-        self.addLink(h8, s4)
-        self.addLink(s1, s21)
-        self.addLink(s21, s2)
-        self.addLink(s1, s10)
-        self.addLink(s2, s10)
-        self.addLink(s3, s11)
-        self.addLink(s4, s22)
-        self.addLink(s11, s4)
-        self.addLink(s3, s22)
-        self.addLink(s21, s17)
-        self.addLink(s11, s17)
-        self.addLink(s10, s18)
-        self.addLink(s22, s18)
+        # Adding the Core Switches
+        switch17 = self.addSwitch('switch17', cls=OVSKernelSwitch)
+        switch18 = self.addSwitch('switch18', cls=OVSKernelSwitch)
 
-topos = { 'mytopo': (lambda: fatTreeTopo() ) }
+        # Adding hosts for the ToR Switch 1
+        host1 = self.addHost('host1', cls=Host, ip='10.1.1.1', defaultRoute=None)
+        host2 = self.addHost('host2', cls=Host, ip='10.1.1.2', defaultRoute=None)
+
+        # Adding hosts for the ToR Switch 2
+        host3 = self.addHost('host3', cls=Host, ip='10.1.1.3', defaultRoute=None)
+        host4 = self.addHost('host4', cls=Host, ip='10.1.1.4', defaultRoute=None)
+
+        # Adding hosts for the ToR Switch 3
+        host5 = self.addHost('host5', cls=Host, ip='10.1.1.5', defaultRoute=None)
+        host6 = self.addHost('host6', cls=Host, ip='10.1.1.6', defaultRoute=None)
+
+        # Adding hosts for the ToR Switch 4
+        host7 = self.addHost('host7', cls=Host, ip='10.1.1.7', defaultRoute=None)
+        host8 = self.addHost('host8', cls=Host, ip='10.1.1.8', defaultRoute=None)
+
+        # Add links
+        self.addLink(host1, switch1)
+        self.addLink(host2, switch1)
+        self.addLink(host3, switch2)
+        self.addLink(host4, switch2)
+        self.addLink(host5, switch3)
+        self.addLink(host6, switch3)
+        self.addLink(host7, switch4)
+        self.addLink(host8, switch4)
+        self.addLink(switch1, switch21)
+        self.addLink(switch21, switch2)
+        self.addLink(switch1, switch10)
+        self.addLink(switch2, switch10)
+        self.addLink(switch3, switch11)
+        self.addLink(switch4, switch22)
+        self.addLink(switch11, switch4)
+        self.addLink(switch3, switch22)
+        self.addLink(switch21, switch17)
+        self.addLink(switch11, switch17)
+        self.addLink(switch10, switch18)
+        self.addLink(switch22, switch18)
+
+topos = {'mytopo': (lambda: FatTreeTopo())}
